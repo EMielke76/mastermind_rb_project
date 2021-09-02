1e8afdc65a3747dd28b479f6f6c7c303c7cdc1d3
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-09-02 08:36:52 -0600

    Feat: ask_for_guess grows larger...

--- lib/game_state.rb
+++ lib/game_state.rb
@@ -28,17 +28,23 @@ class GameState
 #prompts the user to make a guess and change into an array of letters
   def ask_for_guess
     #
-    # puts "I have generated a beginner sequence with four elements made up of:"\
-    # "(r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the"\
-    # " game."
+    puts "I have generated a beginner sequence with four elements made up of:"\
+    "(r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the"\
+    " game."
 
     puts "What's your guess?"
 
     guess_string = gets.chomp
-    @guess = guess_string.split
 
+      if guess_string == ("q" or ("quit" or "Q"))
+        puts "Thanks for playing!"
+      elsif guess_string == ('c' or ("C" or "Cheat"))
+        puts @secret_code
+      else
+        @guess = guess_string.split("")
+      end
 
-    puts "your guess is #{@guess}"
+      puts "your guess is #{@guess}"
 
   end
 
@@ -47,11 +53,12 @@ game1.ask_for_guess
 
 
 # #given @guess, call for appropriate method for response
-  # def respond_to_guess
-  #
-  #   puts "respond to testing"
-  #
-  # end
+  def respond_to_guess
+
+
+    # puts "respond to testing"
+
+  end
 
 # #quit the game if guess == 'q' or 'quit'
 #   def quit
